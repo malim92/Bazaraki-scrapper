5c56c841c43ccad4fe092b4e3de9b56f5dcb58ee
Ali <malim92@live.com>
2024-08-27 15:54:32 +0300

    Initial commit

---
+++ src/main/java/com/bs/bazarakiscrapper/Services/GreetingService.java
@@ -0,0 +1,10 @@
+package com.bs.bazarakiscrapper.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+    public String getGreeting() {
+        return "Hello, Spring Boot and JavaFX!";
+    }
+}
